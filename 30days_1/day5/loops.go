5d88a8dffab708ab0176d3ba1bbe62eab16fbdc5
Darek Dziamalek <ddziamalek@gmail.com>
2017-02-21 23:03:06 +0000

    day5

--- 30days_1/day5/loops.go
+++ 30days_1/day5/loops.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-
+	var input,prod int
+	fmt.Scanf("%d",&input)
+	for x:=1;x<=10;x++ {
+		prod = input * x
+		fmt.Printf("%d x %d = %d\n",input,x,prod)
+	}
 }
