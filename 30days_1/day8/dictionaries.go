4c5d441d49abbb769b9f59f5b1b0a6e2712735c5
Darek Dziamalek <ddziamalek@gmail.com>
2017-02-26 19:45:17 -0500

    day7

--- 30days_1/day8/dictionaries.go
+++ 30days_1/day8/dictionaries.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
-	var input int
+	var input, number int
+	var name string
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Scanf("%d", &input)
-	nums := make([]int, input)
+	phone_book := make(map[string]int, input)
 	for x := 0; x < input; x++ {
-		fmt.Scanf("%d", &nums[x])
+		fmt.Scanf("%s %d", &name, &number)
+		phone_book[name] = number
 	}
-	for x := input - 1; x >= 0; x-- {
-		fmt.Printf("%d ", nums[x])
+	for scanner.Scan() {
+		name = scanner.Text()
+		res := phone_book[name]
+		if res != 0 {
+			fmt.Printf("%s=%d\n", name, res)
+		} else {
+			fmt.Println("Not found")
+		}
 	}
 }
